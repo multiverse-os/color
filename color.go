ec0760898c38dea614411d3d239f7336b2cfca66
Your Name <you@example.com>
2019-09-06 23:40:31 -0400

    color.go: Bringing everything inline with main ANSI package

--- color.go
+++ color.go
@@ -52,7 +52,72 @@ const (
 
 func Sequence(code int) string { return escape + prefix + strconv.Itoa(code) + suffix }
 
-func Color(color int, text string) string { return Sequence(color) + text + Sequence(ColorOff) }
-func Background(color int, text string) string {
-	return Sequence(color+10) + text + Sequence(ColorOff+10)
+func Reset(text string) string         { return Reset.Sequence() + text }
+func ForegroundOff(text string) string { return Sequence(ForegroundOff.Int()) + text }
+func BackgroundOff(text string) string { return Sequence(BackgroundOff.Int()) + text }
+
+func Foreground(code int, text string) string {
+	return Sequence(code) + text + Sequence(ForegroundOff.Int())
 }
+func Background(code int, text string) string {
+	return Sequence(code+10) + text + Sequence(BackgroundOff.Int()+10)
+}
+
+func BackgroundCode(code int) int { return (code + 10) }
+
+func Color(color int, text string) string {
+	switch code := c.Int(); {
+	case Reset.Int():
+		return Reset(text)
+	case (code >= 30 && code <= 37), (code >= 90 && code <= 97):
+		return Color(c, text)
+	case ForegroundOff.Int():
+		return ForegroundOff(text)
+	case BackgroundOff.Int():
+		return BackgroundOff(text)
+	case (code >= 40 && code <= 47), (code >= 100 && code <= 107):
+		return Background(c, text)
+	default:
+		return text
+	}
+}
+
+// Foreground Colorization
+func Black(text string) string        { return Foreground(Black, text) }
+func Red(text string) string          { return Foreground(Red, text) }
+func Green(text string) string        { return Foreground(Green, text) }
+func Yellow(text string) string       { return Foreground(Yellow, text) }
+func Blue(text string) string         { return Foreground(Blue, text) }
+func Magenta(text string) string      { return Foreground(Magenta, text) }
+func Purple(text string) string       { return Foreground(Purple, text) }
+func Cyan(text string) string         { return Foreground(Cyan, text) }
+func Gray(text string) string         { return Foreground(Gray, text) }
+func DarkGray(text string) string     { return Foreground(DarkGray, text) }
+func LightRed(text string) string     { return Foreground(LightGray, text) }
+func LightGreen(text string) string   { return Foreground(LightRed, text) }
+func LightYellow(text string) string  { return Foreground(LightGreen, text) }
+func LightBlue(text string) string    { return Foreground(LightBlue, text) }
+func LightMagenta(text string) string { return Foreground(LightMagenta, text) }
+func LightPurple(text string) string  { return Foreground(LightPurple, text) }
+func LightCyan(text string) string    { return Foreground(LightCyan, text) }
+func White(text string) string        { return Foreground(White, text) }
+
+// Background Colorization
+func BlackBackground(text string) string        { return Background(Black, text) }
+func RedBackground(text string) string          { return Background(Red, text) }
+func GreenBackground(text string) string        { return Background(Green, text) }
+func YellowBackground(text string) string       { return Background(Yellow, text) }
+func BlueBackground(text string) string         { return Background(Blue, text) }
+func MagentaBackground(text string) string      { return Background(Magenta, text) }
+func PurpleBackground(text string) string       { return Background(Purple, text) }
+func CyanBackground(text string) string         { return Background(Cyan, text) }
+func GrayBackground(text string) string         { return Background(Gray, text) }
+func DarkGrayBackground(text string) string     { return Background(DarkGray, text) }
+func LightRedBackground(text string) string     { return Background(LightGray, text) }
+func LightGreenBackground(text string) string   { return Background(LightRed, text) }
+func LightYellowBackground(text string) string  { return Background(LightGreen, text) }
+func LightBlueBackground(text string) string    { return Background(LightBlue, text) }
+func LightMagentaBackground(text string) string { return Background(LightMagenta, text) }
+func LightPurpleBackground(text string) string  { return Background(LightPurple, text) }
+func LightCyanBackground(text string) string    { return Background(LightCyan, text) }
+func WhiteBackground(text string) string        { return Background(White, text) }
